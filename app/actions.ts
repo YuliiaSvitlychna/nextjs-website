82e8d6d94dd0958c44d5ee76dbbee4bf74fbc4ed
Юлія Світлична <uliasvitlicna@Noutbuk-Ulia.local>
2026-03-17 23:58:11 +0200

    add category to blog page component

--- app/actions.ts
+++ app/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { db } from "./db/client";
-import { categories, posts, Post } from "./db/schema";
+import { categories, posts, Post, Category } from "./db/schema";
 import { sql } from "drizzle-orm";
 
 export async function getPostByFullPath(slugs: string[]): Promise<Post | null> {
@@ -46,6 +46,46 @@ export async function getPostByFullPath(slugs: string[]): Promise<Post | null> {
     return rows[0] as Post;
   } catch (error) {
     console.error("Error while searching for post by path:", error);
+    return null;
+  }
+}
+
+export async function getCategoryByFullPath(
+  slugs: string[],
+): Promise<Category | null> {
+  if (slugs.length === 0) return null;
+
+  const fullPath = slugs.join("/");
+
+  try {
+    const rows = await db.execute(sql`
+      WITH RECURSIVE category_tree AS (
+        SELECT 
+            *,
+            slug::text AS full_path
+        FROM ${categories}
+        WHERE parent_id IS NULL
+
+        UNION ALL
+        SELECT 
+            c.*,
+            (ct.full_path || '/' || c.slug)::text
+        FROM ${categories} c
+        JOIN category_tree ct ON c.parent_id = ct.id
+      )
+      SELECT 
+        * FROM category_tree
+      WHERE full_path = ${fullPath}
+      LIMIT 1;
+    `);
+
+    if (!rows || rows.length === 0) {
+      return null;
+    }
+
+    return rows[0] as Category;
+  } catch (error) {
+    console.error("Error while searching for category by path:", error);
     return null;
   }
 }
--- app/blog/[...slugs]/page.tsx
+++ app/blog/[...slugs]/page.tsx
@@ -1,6 +1,6 @@
 "use server";
 
-import { getPostByFullPath } from "../../actions";
+import { getPostByFullPath, getCategoryByFullPath } from "../../actions";
 import { notFound } from "next/navigation";
 
 type PropsType = {
@@ -10,16 +10,24 @@ type PropsType = {
 export default async function Page(props: PropsType) {
   const params = await props.params;
 
-  const post = await getPostByFullPath(params.slugs);
-
-  if (!post) {
-    notFound();
+  const category = await getCategoryByFullPath(params.slugs);
+  if (category) {
+    return (
+      <div>
+        <h1>{category.title}</h1>
+      </div>
+    );
   }
 
-  return (
-    <div className="container">
-      <h1>{post.title}</h1>
-      <div>{post.body}</div>
-    </div>
-  );
+  const post = await getPostByFullPath(params.slugs);
+  if (post) {
+    return (
+      <div className="container">
+        <h1>{post.title}</h1>
+        <div>{post.body}</div>
+      </div>
+    );
+  }
+
+  notFound();
 }
