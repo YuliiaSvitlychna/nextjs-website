1d7a592015d86f592a9567b8a40b672a1c09e492
Юлія Світлична <uliasvitlicna@Noutbuk-Ulia.local>
2026-07-17 03:14:33 +0300

    Blog page layout

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,21 +4,27 @@ import { BLOG_PREFIX } from '@/config';
 export { blogMetadata as metadata } from '@/lib/seo/static';
 import { JsonLd } from '@/components/seo/json-ld';
 import { blogSchema } from '@/lib/seo/static';
-import './page.module.scss';
+import styles from './page.module.scss';
 
 export default async function BlogPage() {
   const categories = await getRootCategories();
 
   return (
     <>
-      <div>
-        <h1>Blog</h1>
-        {categories.map((cat) => (
-          <div key={cat.id}>
-            <Link href={`/${BLOG_PREFIX}/${cat.slug}`}>{cat.title}</Link>
-          </div>
-        ))}
-      </div>
+      <article className={styles.page}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>Blog</h1>
+        </header>
+        <div className={styles.list}>
+          {categories.map((cat) => (
+            <div key={cat.id} className={styles.item}>
+              <Link href={`/${BLOG_PREFIX}/${cat.slug}`} className={styles.link}>
+                {cat.title}
+              </Link>
+            </div>
+          ))}
+        </div>
+      </article>
       <JsonLd schema={blogSchema} />
     </>
   );
